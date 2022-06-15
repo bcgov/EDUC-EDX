a6247e4274a9bad681619261d30f75f59d7224e8
chris.ditcher <Chris.Ditcher@QP.GOV.BC.CA>
2022-06-15 13:14:22 -0700

    Uncommented comments

--- test/src/helpers/secure-exchange-utils.js
+++ test/src/helpers/secure-exchange-utils.js
@@ -15,16 +15,16 @@ const secureExchangeUtils = {
     secureExchange.ministryOwnershipTeamID = test_exchange_object.ministryOwnershipTeamID;
     secureExchange.secureExchangeContactTypeCode = test_exchange_object.secureExchangeContactTypeCode;
     secureExchange.createUser = test_exchange_object.userName;
-    //secureExchange.updateUser = test_exchange_object.userName;
+    secureExchange.updateUser = test_exchange_object.userName;
     secureExchange.secureExchangeStatusCode = test_exchange_object.secureExchangeStatusCode;
-    //secureExchange.reviewer = test_exchange_object.reviewer;
+    secureExchange.reviewer = test_exchange_object.reviewer;
     secureExchange.subject = test_exchange_object.subject;
-    //secureExchange.isReadByMinistry = test_exchange_object.isReadByMinistry;
-    //secureExchange.isReadByExchangeContact = test_exchange_object.isReadByExchangeContact;
+    secureExchange.isReadByMinistry = test_exchange_object.isReadByMinistry;
+    secureExchange.isReadByExchangeContact = test_exchange_object.isReadByExchangeContact;
     comment.commentUserName = test_exchange_object.userName;
     comment.content = test_exchange_object.commentContent;
     comment.createUser = test_exchange_object.userName;
-    //comment.updateUser = test_exchange_object.userName;
+    comment.updateUser = test_exchange_object.userName;
     secureExchange.addComment(comment);
     return secureExchange;
   },
