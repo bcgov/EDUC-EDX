7359680b6ec022575b5e04b8402b4a944cee770f
SoLetsDev <derek.so@gov.bc.ca>
2022-06-15 15:17:02 -0700

    addressing bug edx-191 mark as read also adjusted tests with some quality of life adjustments

--- test/src/test_cases/edx-schools-frontend/test-school-message-display.js
+++ test/src/test_cases/edx-schools-frontend/test-school-message-display.js
@@ -37,6 +37,7 @@ fixture`school-message-display`
 
 test('test-school-message-display', async t => {
     //Verify header information.
+    log.info('verifying header information');
     await t.navigateTo(base_url + '/exchange/' + testExchange.secureExchangeID);
     await t.expect(messageDisplay.navTitle.innerText).contains('View Message');
     await t.expect(messageDisplay.subjectHeading.innerText).eql(testExchange.subject);
@@ -45,23 +46,21 @@ test('test-school-message-display', async t => {
     await t.expect((await messageDisplay.secureExchangeStatusCode.innerText).toLowerCase()).eql(testExchange.secureExchangeStatusCode.toLowerCase());
     await t.expect(messageDisplay.sequenceNumber.innerText).eql(testExchange.sequenceNumber);
 
-    //Verify action buttons
-    await t.expect(((testExchange.secureExchangeStatusCode === 'COMPLETE') && (messageDisplay.editOptionsMenu.exists))).notOk();
-    await t.expect(((testExchange.secureExchangeStatusCode !== 'COMPLETE') && (messageDisplay.editOptionsMenu.exists))).ok();
-    //The mark as button text reflects the read state to which the secure exchange will be set.
-    //IE: the button reading Unread signifies that the secure exchange will be marked as unread when the button is clicked.
-    await t.expect(messageDisplay.markAsSpan.innerText).eql('Unread');
-    testExchange.isReadByExchangeContact = true;
-    await t.click(messageDisplay.markAsButton);
-    await t.expect(messageDisplay.markAsSpan.innerText).eql('Read');
-    await t.click(messageDisplay.markAsButton);
-    await t.expect(messageDisplay.markAsSpan.innerText).eql('Unread');
-
     //Verify action timeline
     if (testExchange.commentsList.length > 0) {
+        log.info('verifying edx message timeline')
         await t.expect(messageDisplay.lastActivity.exists).ok();
         await t.expect((await messageDisplay.activityTitle.innerText).length > 0).ok();
         await t.expect((await messageDisplay.activityDisplayDate.innerText).length > 0).ok();
         await t.expect(await messageDisplay.activityContent.innerText).eql(testExchange.commentsList[testExchange.commentsList.length - 1].content);
     }
+
+    //Verify action buttons
+    log.info('verifying message display buttons');
+    await t.expect(((testExchange.secureExchangeStatusCode === 'COMPLETE') && (messageDisplay.editOptionsMenu.exists))).notOk();
+    await t.expect(((testExchange.secureExchangeStatusCode !== 'COMPLETE') && (messageDisplay.editOptionsMenu.exists))).ok();
+    await t.expect(messageDisplay.markAsSpan.innerText).eql('Mark As Unread');
+    await t.click(messageDisplay.markAsButton);
+    //once we mark message as unread, we should go back to the inbox page.
+    await t.expect(messageDisplay.navTitle.innerText).contains('Secure Exchange Inbox');
 });
