a733197545a9a5ef1afff0ea393bab35804af878
arcshiftsolutions <arcshiftsolutions@gmail.com>
2022-06-24 13:44:27 -0700

    Small fix for disable unread.

--- frontend/src/components/SecureExchange/MessageDisplay.vue
+++ frontend/src/components/SecureExchange/MessageDisplay.vue
@@ -73,7 +73,7 @@
                 </v-card>
               </v-speed-dial>
               <v-spacer></v-spacer>
-              <v-btn id="markAsButton" :loading="loadingReadStatus" class="ma-4" v-on:click="clickMarkAsButton">
+              <v-btn :disabled="!isEditable()" id="markAsButton" :loading="loadingReadStatus" class="ma-4" v-on:click="clickMarkAsButton">
                 <v-icon v-if="secureExchange.isReadByExchangeContact">mdi-email-outline</v-icon>
                 <v-icon v-else>mdi-email-open-outline</v-icon>
                 <span class="ml-1 markAsSpan">Mark As {{ secureExchange.isReadByExchangeContact ? 'Unread' : 'Read' }}</span>
