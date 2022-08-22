a34af46f5ff4e88d613fffeec810dba4556bcc7a
keeganbcgov <Keegan.Laur@gov.bc.ca>
2022-08-22 15:00:36 -0700

    Sending an empty GUID to filter out students when a PEN isn't found.

--- backend/src/components/secureExchange.js
+++ backend/src/components/secureExchange.js
@@ -161,13 +161,9 @@ async function getExchangesPaginated(req) {
   if (req.query.searchParams) {
     parsedParams = JSON.parse(req.query.searchParams);
     if(parsedParams.studentPEN){
-      let studentDetail = await getData(accessToken, config.get('student:apiEndpoint') + '/?pen=' + parsedParams.studentPEN);
-      if(studentDetail[0]){
-        parsedParams.studentId = studentDetail[0].studentID;
-        delete parsedParams.studentPEN;
-      }else{
-        return '';
-      }
+      let studentDetail = await getData(accessToken, `${config.get('student:apiEndpoint')}/?pen=${parsedParams.studentPEN}`);
+      parsedParams.studentId = studentDetail[0] ? studentDetail[0].studentID : '00000000-0000-0000-0000-000000000000';
+      delete parsedParams.studentPEN;
     }
   }
   criteria = buildSearchParams(JSON.stringify(parsedParams));
