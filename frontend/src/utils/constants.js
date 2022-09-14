8b1bfcce978deb8fb5bbf2adcd9e72e45431b779
arcshiftsolutions <arcshiftsolutions@gmail.com>
2022-09-14 12:36:11 -0700

    Added district details.

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -2,6 +2,7 @@ const baseRoot = '/api';
 const authRoot = baseRoot + '/auth';
 const edxRoot = baseRoot + '/edx';
 const schoolRoot = baseRoot + '/schools';
+const districtRoot = baseRoot + '/districts';
 const studentRequestRoot = baseRoot + '/student';
 const districtRequestRoot = baseRoot + '/institute/districts';
 const schoolRequestRoot = baseRoot + '/institute/schools';
@@ -32,6 +33,9 @@ export const ApiRoutes = Object.freeze({
     ALL_DIS_SCHOOLS: schoolRoot + '/allSchools',
     ALL_CACHE_SCHOOLS: schoolRoot + '/allCachedSchools',
     SCHOOL_DETAILS_BY_ID: schoolRoot + '/schoolDetailsById',
+  },
+  district: {
+    BASE_URL: districtRoot
   },
   DISTRICT_DATA_URL: districtRequestRoot,
   SCHOOL_DATA_URL: schoolRequestRoot,
