db4b64bd3cb15f0175a104f2700cd8abfbdb4e87
sukanya.rath <sukanyarath1987@gmail.com>
2022-09-14 21:37:15 -0700

    Test -automation - Added single user logic for all test cases. Fixed some broken automation tests and new automation tests for district user activation.

--- test/src/test_suites/regression.js
+++ test/src/test_suites/regression.js
@@ -11,7 +11,7 @@ createTestCafe('localhost', 1337, 1338)
           //FIX ME tests commented out need to be fixed
           /* .src(["src/test_cases/edx-schools-frontend/test-login-page-loads.js",
              "src/test_cases/edx-schools-frontend/test-school-inbox.js","src/test_cases/edx-schools-frontend/test-school-message-display.js"])
- */            .src(["src/test_cases/edx-new-user-invite/",
+ */            .src(["src/test_cases/edx-new-user-invite/","src/test_cases/edx-schools-frontend/test-new-message.js",
             "src/test_cases/edx-user-activation/school/","src/test_cases/edx-user-activation/district/"])
           .run();
     })
