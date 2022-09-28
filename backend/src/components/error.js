e068fe07ebf93e0d950d97b18dee46ca64b45ca3
arcshiftsolutions <arcshiftsolutions@gmail.com>
2022-09-27 17:27:02 -0700

    Test PR.

--- backend/src/components/error.js
+++ backend/src/components/error.js
@@ -2,23 +2,6 @@
 
 const HttpStatus = require('http-status-codes');
 
-class ApiError extends Error {
-  constructor(status = HttpStatus.INTERNAL_SERVER_ERROR, data, reason, ...params) {
-    super(...params);
-
-    // Maintains proper stack trace for where our error was thrown (only available on V8)
-    // if (Error.captureStackTrace) {
-    //   Error.captureStackTrace(this, ApiError);
-    // }
-
-    this.name = 'ApiError';
-    this.status = status;
-    this.data = data;
-    if(reason) {
-      this.stack += '\nCaused By:\n' + reason.stack;
-    }
-  }
-}
 
 class ServiceError extends Error {
   constructor(errorSource, reason, ...params) {
