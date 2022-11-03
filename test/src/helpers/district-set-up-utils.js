2571de48af1278c0b6db56732a384333f230803e
Tim Osse <tim.osse@cgi.com>
2022-11-03 13:50:46 -0700

    EDX-562: AT: EDX-315: DIS - Add New District Contact - replaced loop with filter.

--- test/src/helpers/district-set-up-utils.js
+++ test/src/helpers/district-set-up-utils.js
@@ -38,19 +38,11 @@ const districtSetUpUtils = {
         let districtSuperintendent = '';
         let districtDetails = await districtSetUpUtils.getDistrictDetails(districtNumber);
         let districtID = districtDetails.districtId;
-        for (const disContact of districtDetails.contacts){
-            if(disContact.districtContactTypeCode === 'SUPER'){
-                let parsedExpiryDate = null;
-                if (disContact.expiryDate) {
-                    parsedExpiryDate = new LocalDate.parse(disContact.expiryDate, DateTimeFormatter.ofPattern('uuuu-MM-dd\'T\'HH:mm:ss'));
-                }
-                if (parsedExpiryDate === null && disContact.firstName === 'Tony' && disContact.lastName === 'Hawk') {
-                    districtSuperintendent = disContact;
-                }
-            }
-        }
 
-        let contactID = districtSuperintendent.districtContactId;
+        districtSuperintendent = districtDetails.contacts.filter(contact => contact.districtContactTypeCode === 'SUPER'
+            && contact.expiryDate === null && contact.firstName === 'Tony' && contact.lastName === 'Hawk');
+
+        let contactID = districtSuperintendent[0].districtContactId;
         const url = `${constants.institute_base_url}/api/v1/institute/district/${districtID}/contact/${contactID}`;
         return await restUtils.deleteData(token, url);
     }
