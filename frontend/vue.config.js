cab8bf5f76683ce18239658f50703a9808387883
SoLetsDev <derek.so@gov.bc.ca>
2022-11-09 12:18:00 -0800

    trying out webpack plugin with node global

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,14 @@
 //const path = require('path');
+import webpack from 'webpack';
+
 module.exports = {
   configureWebpack: {
+    node: {
+      global: false
+    },
+    plugins: [new webpack.DefinePlugin({
+      global: 'window'		// Placeholder for global used in any node_modules
+    })],
     devtool: 'inline-source-map',
     module: {
       rules: [
