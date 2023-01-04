7b92fc8c60b735651361753dbf5618bcb5bd98ab
SoLetsDev <74216496+SoLetsDev@users.noreply.github.com>
2023-01-04 11:09:04 -0800

    Increasing user activation test stability.

--- test/src/test_cases/edx-user-activation/school/test-user-school-activation-success-scenario.js
+++ test/src/test_cases/edx-user-activation/school/test-user-school-activation-success-scenario.js
@@ -1,8 +1,9 @@
-import {Selector} from 'testcafe';
-import {base_url} from '../../../config/constants';
 const log = require('npmlog');
 const {getToken} = require('../../../helpers/oauth-utils');
 const {deleteActivationCode,setUpDataForUserActivation,submitDetailsOnUserActivationForm,login, deleteEdxUser} = require('../../../services/edx-api-service');
+import Dashboard from '../../../page_models/dashboard';
+
+const dashboard = new Dashboard();
 
 fixture`edx-user-activate-success-scenario`
   .before(async ctx => {
@@ -14,18 +15,13 @@ fixture`edx-user-activate-success-scenario`
     await deleteActivationCode(data.access_token, ctx.acCode1);
 
   }).beforeEach(async t => {
-  // log in as studentAdmin
   await t.resizeWindow(1920, 1080);
 });
 
 
 test('when_url_visited_user_redirected_to_login_page_and_db_updated', async t => {
-  const getLocation = await login(t);
+  await login(t);
   await submitDetailsOnUserActivationForm(t, '99899998', t.fixtureCtx.primaryCode, t.fixtureCtx.personalCode);
-  await t.wait(5000)
-  const text = await Selector('#mainSnackBar').innerText;
-  await t.expect(text).contains('User Activation Completed Successfully. Redirecting to your Dashboard...');
-  await t.wait(5000)
-    .expect(getLocation()).contains(base_url);
+  await t.expect(dashboard.schoolInboxCard.exists).ok({timeout: 60000});
   log.info('User could activate account successfully');
 });
