0518478c4bf8eeb901f6993d7a1c97b3b3905551
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2023-01-30 13:48:56 -0800

    remove classes for selector and updated id's

--- frontend/src/components/SecureExchange/AccessUserCard.vue
+++ frontend/src/components/SecureExchange/AccessUserCard.vue
@@ -10,7 +10,7 @@
                   <strong>{{ `${user.firstName} ${user.lastName}` }}</strong>
                 </v-col>
                 <v-col cols="2" class="d-flex justify-end" v-if="isNotSameEdxUser()">
-                  <v-btn :id="`user-edit-button-${user.firstName}-${user.lastName}`"
+                  <v-btn :id="`user-edit-button-${user.edxUserID}`"
                          title="Edit"
                          color="white"
                          width="0.5em"
@@ -18,11 +18,11 @@
                          depressed
                          @click="clickEditButton"
                          small
-                         class="mr-2 editEdxUserButton"
+                         class="mr-2"
                   >
                     <v-icon size="x-large" color="#003366" dark>mdi-pencil</v-icon>
                   </v-btn>
-                  <v-btn :id="`user-remove-button-${user.firstName}-${user.lastName}`"
+                  <v-btn :id="`user-remove-button-${user.edxUserID}`"
                          title="Remove"
                          color="white"
                          width="0.5em"
@@ -30,11 +30,11 @@
                          depressed
                          @click="clickDeleteButton"
                          small
-                         class="mr-2 removeEdxUserButton"
+                         class="mr-2"
                   >
                     <v-icon size="x-large" color="#003366" dark>mdi-delete</v-icon>
                   </v-btn>
-                  <v-btn :id="`user-relink-button-${user.firstName}-${user.lastName}`"
+                  <v-btn :id="`user-relink-button-${user.edxUserID}`"
                          title="Re-Link"
                          color="white"
                          width="0.5em"
@@ -42,7 +42,6 @@
                          depressed
                          @click="clickRelinkButton"
                          small
-                         class="relinkEdxUserButton"
                   >
                     <v-icon size="x-large" color="#003366" dark>mdi-backup-restore</v-icon>
                   </v-btn>
