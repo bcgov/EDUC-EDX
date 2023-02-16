149581a2e1bd09dd4238116720533584c5b81f25
Trevor Richards <trev@trevdev.ca>
2023-02-16 13:59:06 -0800

    implement deleteUserActivationCodes in relink test

--- test/src/helpers/user-activation-utils.js
+++ test/src/helpers/user-activation-utils.js
@@ -2,7 +2,9 @@ import EdxActivationRole from '../model/EdxActivationRole';
 import EdxActivationCode from '../model/EdxActivationCode';
 import date from 'date-and-time';
 
-const userActivationUtils = {
+const restUtils = require('../helpers/rest-utils');
+const { getToken } = require('../helpers/oauth-utils');
+const { edx_api_base_url } = require('../config/constants');
 
 const userActivationUtils = {
   createEdxActivationCode(isPrimary, roles, activationCode, instituteTypeCode, instituteID) {
@@ -33,8 +35,17 @@ const userActivationUtils = {
       edxActivationCode.districtID = instituteID;
     }
     return edxActivationCode;
+  },
+
+  /**
+   * @param {string} userID
+   * @returns {Promise<any>} Response data
+   */
+  async deleteUserActivationCodes(userID) {
+    const { access_token } = await getToken();
+    const endPoint = `${edx_api_base_url}api/v1/edx/users/activation-code/user/${userID}`;
+    return await restUtils.deleteData(access_token, endPoint);
   }
 };
-
 
 module.exports = userActivationUtils;
