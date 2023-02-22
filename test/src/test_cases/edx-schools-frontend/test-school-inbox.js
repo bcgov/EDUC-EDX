37d34504c77fc94f913779af7ba012a0db156771
SoLetsDev <74216496+SoLetsDev@users.noreply.github.com>
2023-02-22 11:05:24 -0800

    trying to add forced screenshot right before test failure

--- test/src/test_cases/edx-schools-frontend/test-school-inbox.js
+++ test/src/test_cases/edx-schools-frontend/test-school-inbox.js
@@ -66,6 +66,7 @@ test('test-school-inbox', async t => {
     await inbox.selectStatus('Open');
     // search
     await inbox.clickSearchButton();
+    await t.takeScreenshot();
     // check that our exchange is found by subject heading
     await confirmMessage(t);
     // select the date
