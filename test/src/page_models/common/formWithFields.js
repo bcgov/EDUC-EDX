dac052be45c7c327c8bf17a63e6dd622829b5031
Trevor Richards <trev@trevdev.ca>
2023-02-24 15:32:17 -0800

    single quote strings

--- test/src/page_models/common/formWithFields.js
+++ test/src/page_models/common/formWithFields.js
@@ -1,5 +1,5 @@
-import { t, Selector } from "testcafe";
-import log from "npmlog";
+import { t, Selector } from 'testcafe';
+import log from 'npmlog';
 
 class FormWithFields {
   /** @param {string} submitSelector */
@@ -12,8 +12,8 @@ class FormWithFields {
    * @param {string} [name] - A name for the npmlog
    */
   async verifyFieldIsValid(field, name) {
-    await t.expect(field.parent("div.v-input").hasClass("error--text")).notOk();
-    log.info("pass:", name ? `${name} field is valid` : "field is valid");
+    await t.expect(field.parent('div.v-input').hasClass('error--text')).notOk();
+    log.info('pass:', name ? `${name} field is valid` : 'field is valid');
   }
 
   /**
@@ -21,8 +21,8 @@ class FormWithFields {
    * @param {string} [name] - A name for the npmlog
    */
   async verifyFieldIsInvalid(field, name) {
-    await t.expect(field.parent("div.v-input").hasClass("error--text")).ok();
-    log.info("pass:", name ? `${name} field is invalid` : "field is invalid");
+    await t.expect(field.parent('div.v-input').hasClass('error--text')).ok();
+    log.info('pass:', name ? `${name} field is invalid` : 'field is invalid');
   }
 
   /**
@@ -30,18 +30,18 @@ class FormWithFields {
    * @param {string} [name] - A name for the npmlog
    */
   async clearTextField(field, name) {
-    await t.selectText(field).pressKey("delete");
-    log.info( "input:", name ? `${name} field has been cleared` : "field has been cleared");
+    await t.selectText(field).pressKey('delete');
+    log.info( 'input:', name ? `${name} field has been cleared` : 'field has been cleared');
   }
 
   async verifySubmitIsDisabled() {
     await t.expect(this.submitButton.hasAttribute('disabled')).ok();
-    log.info('pass:', 'The submit button is disabled')
+    log.info('pass:', 'The submit button is disabled');
   }
 
   async verifySubmitIsEnabled() {
     await t.expect(this.submitButton.hasAttribute('disabled')).notOk();
-    log.info('pass:', 'The submit button is enabled')
+    log.info('pass:', 'The submit button is enabled');
   }
 }
 
