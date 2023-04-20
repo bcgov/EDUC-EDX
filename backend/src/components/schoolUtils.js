172059d86603906f3d939bb63cf5f4843a4f4f77
arcshiftsolutions <arcshiftsolutions@gmail.com>
2023-04-19 17:50:44 -0700

    Small fix for active/inactive schools

--- backend/src/components/schoolUtils.js
+++ backend/src/components/schoolUtils.js
@@ -16,7 +16,8 @@ function isSchoolActive(school) {
   const currentTime = LocalDate.now();
   const openedDate = school?.effectiveDate;
   const closedDate = school?.expiryDate;
-  return !(!school || !school.schoolName || !openedDate || currentTime.isBefore(LocalDate.parse(openedDate, DateTimeFormatter.ISO_LOCAL_DATE_TIME)) || (closedDate && currentTime.isAfter(LocalDate.parse(closedDate, DateTimeFormatter.ISO_LOCAL_DATE_TIME))));
+  return school && school.schoolName && !!openedDate && !currentTime.isBefore(LocalDate.parse(openedDate, DateTimeFormatter.ISO_LOCAL_DATE_TIME))
+      && (!closedDate || currentTime.isAfter(LocalDate.parse(closedDate, DateTimeFormatter.ISO_LOCAL_DATE_TIME)));
 }
 
 module.exports = {
