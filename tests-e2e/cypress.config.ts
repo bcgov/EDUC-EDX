067a3a538ca22e4c7267f4230d45e59c54dd9c2b
arcshiftsolutions <arcshiftsolutions@gmail.com>
2023-04-21 11:19:40 -0700

    Small fixes.

--- tests-e2e/cypress.config.ts
+++ tests-e2e/cypress.config.ts
@@ -5,7 +5,7 @@ module.exports = defineConfig({
   fixturesFolder: 'cypress/fixtures',
   screenshotsFolder: 'cypress/screenshots',
   videosFolder: 'cypress/videos',
-  video: true,
+  video: false,
   screenshotOnRunFailure: false,
   e2e: {
     baseUrl: 'https://dev.educationdataexchange.gov.bc.ca',
