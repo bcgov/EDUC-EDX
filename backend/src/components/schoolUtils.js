b2381f03310692e1ac62760fab0f2313f43c44c6
arcshiftsolutions <arcshiftsolutions@gmail.com>
2023-05-16 10:31:43 -0700

    Small fix.

--- backend/src/components/schoolUtils.js
+++ backend/src/components/schoolUtils.js
@@ -17,7 +17,7 @@ function isSchoolActive(school) {
   const openedDate = school?.effectiveDate;
   const closedDate = school?.expiryDate;
   return school && school.schoolName && !!openedDate && !currentTime.isBefore(LocalDate.parse(openedDate, DateTimeFormatter.ISO_LOCAL_DATE_TIME))
-      && (!closedDate || currentTime.isAfter(LocalDate.parse(closedDate, DateTimeFormatter.ISO_LOCAL_DATE_TIME)));
+      && (!closedDate || currentTime.isBefore(LocalDate.parse(closedDate, DateTimeFormatter.ISO_LOCAL_DATE_TIME)));
 }
 
 module.exports = {
