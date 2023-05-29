3d8e1d632e41fb45e9fc3773bcdd1f866ca1a88b
Trevor Richards <trev@trevdev.ca>
2023-05-29 09:42:15 -0700

    fix: class that should just be an obj
    The typings for a simple object of constants does not work well if each
    property needs to be typed as 'any'

--- tests-e2e/cypress/support/selectors.ts
+++ tests-e2e/cypress/support/selectors.ts
@@ -1,46 +1,39 @@
-export class Selectors {
-
-    dashboard: any = {
-        dataCollectionsTile: '#studentDataCollectionCard',
-        dataCollectionsTileTitle: '#studentDataCollectionCard > div.v-row.pl-4 > div.v-col.mt-2 > div:nth-child(1) > div > h4',
-        title: '#navTitle > div',
-    };
-
-    dataCollectionsLanding: any = {
-        continue: '.navigate',
-        title: '#navTitle > div',
-    };
-
-    schoolContacts: any = {
-        listItem: '.v-list-item',
-        newContactButton: '#addSchoolContactBtn',
-        newContactCalendar: '.dp__outer_menu_wrap',
-        newContactEffectiveDateTextField: '#newContactEffectiveDateTextField',
-        newContactEmailInput: '#newContactEmailInput',
-        newContactLastNameInput: '#newContactLastNameInput',
-        newContactPhoneNumberInput: '#newContactPhoneNumberInput',
-        newContactPostBtn: '#newContactPostBtn',
-        newContactTypeDropdown: '#newContactDropdown',
-        subjectHeading: 'h2.subjectHeading',
-    };
-
-    schoolDetails: any = {
-        editButton: '#schoolDetailsEditButton',
-        resolveBtn: '#resolveBtn',
-        saveButton: '#saveButton',
-        schoolDetailsEmail: '#schoolDetailsEmail',
-        schoolDetailsPhoneNumber: '#schoolDetailsPhoneNumber',
-        subjectHeading: 'h2.subjectHeading',
-
-    };
-
-    stepOneSchoolDetails: any = {
-        formHint: 'p.form-hint',
-        nextButton: '#nextButton',
-    };
-
-    stepTwoSchoolContacts: any = {
-        formHint: 'p.form-hint',
-        nextButton: '#nextButton',
-    };
+export default {
+  dashboard: {
+    dataCollectionsTile: '#studentDataCollectionCard',
+    dataCollectionsTileTitle: '#studentDataCollectionCard > div.v-row.pl-4 > div.v-col.mt-2 > div:nth-child(1) > div > h4',
+    title: '#navTitle > div',
+  },
+  dataCollectionsLanding: {
+    continue: '.navigate',
+    title: '#navTitle > div',
+  },
+  schoolContacts: {
+    listItem: '.v-list-item',
+    newContactButton: '#addSchoolContactBtn',
+    newContactCalendar: '.dp__outer_menu_wrap',
+    newContactEffectiveDateTextField: '#newContactEffectiveDateTextField',
+    newContactEmailInput: '#newContactEmailInput',
+    newContactLastNameInput: '#newContactLastNameInput',
+    newContactPhoneNumberInput: '#newContactPhoneNumberInput',
+    newContactPostBtn: '#newContactPostBtn',
+    newContactTypeDropdown: '#newContactDropdown',
+    subjectHeading: 'h2.subjectHeading',
+  },
+  schoolDetails: {
+    editButton: '#schoolDetailsEditButton',
+    resolveBtn: '#resolveBtn',
+    saveButton: '#saveButton',
+    schoolDetailsEmail: '#schoolDetailsEmail',
+    schoolDetailsPhoneNumber: '#schoolDetailsPhoneNumber',
+    subjectHeading: 'h2.subjectHeading',
+  },
+  stepOneSchoolDetails: {
+    formHint: 'p.form-hint',
+    nextButton: '#nextButton',
+  },
+  stepTwoSchoolContacts: {
+    formHint: 'p.form-hint',
+    nextButton: '#nextButton',
+  }
 }
