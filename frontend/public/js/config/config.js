3957bc1f3f0ad2e1b8baf48a164973f3457a0207
SoLetsDev <74216496+SoLetsDev@users.noreply.github.com>
2023-05-29 14:52:49 -0700

    Reverting changes back to original.

--- frontend/public/js/config/config.js
+++ frontend/public/js/config/config.js
@@ -1,11 +1,8 @@
-let publicStaticConfig = function() {
-  return {
-    VUE_APP_BCEID_REG_URL: 'https://www.test.bceid.ca/os/?7081&SkipTo=Basic#action',
-    VUE_APP_META_DATA: [ { name: 'robots', content: 'noindex,nofollow' } ],
-    BANNER_ENVIRONMENT:'DEV',
-    BANNER_COLOR:'#8d28d7'
-  };
+var publicStaticConfig = {
+  VUE_APP_BCEID_REG_URL: 'https://www.test.bceid.ca/os/?7081&SkipTo=Basic#action',
+  VUE_APP_META_DATA: [ { name: 'robots', content: 'noindex,nofollow' } ],
+  BANNER_ENVIRONMENT:'DEV',
+  BANNER_COLOR:'#8d28d7'
 };
-
 export default publicStaticConfig;
 
