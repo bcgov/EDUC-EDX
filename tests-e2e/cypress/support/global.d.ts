71f0a3a199ef3f81dfd3fa391c633fe55a914e02
Trevor Richards <trev@trevdev.ca>
2023-06-02 10:34:40 -0700

    feat: add overload for typing dataLoad options

--- tests-e2e/cypress/support/global.d.ts
+++ tests-e2e/cypress/support/global.d.ts
@@ -1,5 +1,11 @@
-declare namespace Cypress {
-    interface Chainable<Subject> {
-        login(): Chainable<any>
+import { AppSetupData } from "tests-e2e/cypress.config";
+import { InstituteSetupOptions } from "../helpers/institute-set-up-utils";
+
+declare global {
+    namespace Cypress {
+        interface Chainable<Subject> {
+            login(): Chainable<any>;
+            task<S = InstituteSetupOptions, T = AppSetupData>(event: string, arg?: S): Chainable<T>
+        }
     }
 }
