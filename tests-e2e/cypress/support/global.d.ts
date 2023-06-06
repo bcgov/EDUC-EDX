91298eac2bdf7cb84d1afec53e3710bcc42fc0a3
Trevor Richards <trev@trevdev.ca>
2023-06-06 16:09:10 -0700

    feat: overflow for task generic and custom options
    Adds a generic overflow to the `.task()` generic that accepts some options
    and returns a generic return type for the argument passed into the
    promise callback function of `.then((arg) => {...})`

--- tests-e2e/cypress/support/global.d.ts
+++ tests-e2e/cypress/support/global.d.ts
@@ -1,11 +1,11 @@
 import { AppSetupData } from "tests-e2e/cypress.config";
-import { InstituteSetupOptions } from "../helpers/institute-set-up-utils";
+import { InstituteOptions } from "../services/institute-api-service";
 
 declare global {
     namespace Cypress {
         interface Chainable<Subject> {
             login(): Chainable<any>;
-            task<S = InstituteSetupOptions, T = AppSetupData>(event: string, arg?: S): Chainable<T>
+            task<S = InstituteOptions, T = AppSetupData>(event: string, arg?: S): Chainable<T>
         }
     }
 }
