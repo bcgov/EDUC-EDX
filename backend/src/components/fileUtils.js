28a6bf599c14fee7889b53dce88f1a8a8980765a
arcshiftsolutions <arcshiftsolutions@gmail.com>
2023-06-12 08:56:49 -0700

    Text updates.

--- backend/src/components/fileUtils.js
+++ backend/src/components/fileUtils.js
@@ -20,15 +20,16 @@ async function scanFilePayload(req, res, next) {
 async function scanFile(base64File){
   try{
     const ClamAVScanner = createScanner(config.get('clamav:host'), Number(config.get('clamav:port')));
-    console.log('ClamAV scanning file ' + base64File);
     const clamAVScanResult = await ClamAVScanner.scanBuffer(Buffer.from(base64File, 'base64'), 3000, 1024 * 1024);
     if (clamAVScanResult.includes('FOUND')) {
+      console.log('ClamAV scan found possible virus');
       return false;
     }
   } catch (e) {
     // if virus scan is not to be performed/cannot be performed
     console.log('ClamAV Scanner was not found: ' + e);
   }
+  console.log('ClamAV scan found no virus in file, allowing upload...');
   return true;
 }
 
