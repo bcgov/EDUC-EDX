98db0fc3f5f772c19aa6e371500732a6dd0c127a
Trevor Richards <trev@trevdev.ca>
2023-06-15 16:10:18 -0700

    feat: break accessDistrictUsers into contexts.

--- tests-e2e/cypress/e2e/edx-frontend-district/accessDistrictUsers.cy.ts
+++ tests-e2e/cypress/e2e/edx-frontend-district/accessDistrictUsers.cy.ts
@@ -2,39 +2,42 @@ import { AppSetupData } from "tests-e2e/cypress.config";
 import { InstituteOptions } from "tests-e2e/cypress/services/institute-api-service";
 import selectors from "../../support/selectors";
 
-before(() => {
-  cy.task<InstituteOptions, AppSetupData>('dataLoad');
-  cy.task<DistrictUserOptions>('setup-districtUser', {
-    districtRoles: ['EDX_DISTRICT_ADMIN'],
-    districtCodes: ['998']
-  });
-  cy.login();
-});
-
-after(() => cy.logout());
 
 describe('Access District Users Page Tests', () => {
-  it('Generates an initial primary code', () => {
-    cy.intercept(Cypress.env('interceptors').activation_code).as('activationCodeUpdate');
-
-    cy.visit('/districtAccess');
-    cy.wait('@activationCodeUpdate');
-
-    cy.get(selectors.newUserInvites.primaryActivationCode).invoke('text').as('initialCode');
-    cy.get('@initialCode').then(initialCode => {
-      cy.get(selectors.newUserInvites.toggleGenerateNewCode).click();
-      cy.get(selectors.newUserInvites.generateNewCode).click();
-
-      cy.wait('@activationCodeUpdate').then(({response}) => {
-        expect(response?.body.activationCode).not.null;
-        expect(response?.body.activationCode).not.eq(initialCode);
+  context('as an edx district administrator', () => {
+    before(() => {
+      cy.task<InstituteOptions, AppSetupData>('dataLoad');
+      cy.task<DistrictUserOptions>('setup-districtUser', {
+        districtRoles: ['EDX_DISTRICT_ADMIN'],
+        districtCodes: ['998']
       });
-
-      cy.get(selectors.newUserInvites.primaryActivationCode).invoke("text").then(newCode => {
-        cy.get(selectors.snackbar.mainSnackBar)
-          .should('include.text', `The new Primary Activation Code is ${newCode}. Close`);
-        expect(initialCode).not.eq(newCode);
-      })
+      cy.login();
     });
-  });
+
+    after(() => cy.logout());
+
+    it('should be able to generate primary access codes', () => {
+      cy.intercept(Cypress.env('interceptors').activation_code).as('activationCodeUpdate');
+
+      cy.visit('/districtAccess');
+      cy.wait('@activationCodeUpdate');
+
+      cy.get(selectors.newUserInvites.primaryActivationCode).invoke('text').as('initialCode');
+      cy.get('@initialCode').then(initialCode => {
+        cy.get(selectors.newUserInvites.toggleGenerateNewCode).click();
+        cy.get(selectors.newUserInvites.generateNewCode).click();
+
+        cy.wait('@activationCodeUpdate').then(({response}) => {
+          expect(response?.body.activationCode).not.null;
+          expect(response?.body.activationCode).not.eq(initialCode);
+        });
+
+        cy.get(selectors.newUserInvites.primaryActivationCode).invoke("text").then(newCode => {
+          cy.get(selectors.snackbar.mainSnackBar)
+            .should('include.text', `The new Primary Activation Code is ${newCode}. Close`);
+          expect(initialCode).not.eq(newCode);
+        })
+      });
+    });
+  })
 })
