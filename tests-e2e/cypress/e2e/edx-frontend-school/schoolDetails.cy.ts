c99bba8d3ca412841961864f3a02048f975c623f
Trevor Richards <trev@trevdev.ca>
2023-06-19 09:27:40 -0700

    chore: refactor to BDD context

--- tests-e2e/cypress/e2e/edx-frontend-school/schoolDetails.cy.ts
+++ tests-e2e/cypress/e2e/edx-frontend-school/schoolDetails.cy.ts
@@ -2,42 +2,41 @@ import selectors from '../../support/selectors';
 import { AppSetupData } from '../../../cypress.config';
 import { vInputParentOf } from 'tests-e2e/cypress/support/utils';
 
-before(() => {
-  cy.task<AppSetupData>('dataLoad').then(() => {
-    cy.task('setup-schoolUser', ['99998']);
-  });
-  cy.login();
-})
-
-after(() => {
-  cy.logout();
-})
-
 describe('School Details Interface Test', () => {
-  it('Loads school details and checks field validation', () => {
-    cy.visit('/')
-    cy.get(selectors.dashboard.title, {timeout: 60000}).contains('Dashboard | EDX Automation Testing School');
-    cy.get(selectors.dashboard.schoolDetailsCard).click();
-    cy.get(selectors.schoolDetails.addWebsiteLink).click();
+  context('As an EDX school user', () => {
+    before(() => {
+      cy.task<AppSetupData>('dataLoad').then(() => {
+        cy.task('setup-schoolUser', ['99998']);
+      });
+    })
+    beforeEach(() => cy.login());
+    after(() => cy.logout());
 
-    const websiteField = () => cy.get(selectors.schoolDetails.schoolDetailsWebsite);
-    const websiteWrapper = vInputParentOf(websiteField);
-    const websiteErrorMessage = 'Website must be valid and secure (i.e., https)';
+    it('can load the details page and validate the website url field', () => {
+      cy.visit('/')
+      cy.get(selectors.dashboard.title, {timeout: 60000}).contains('Dashboard | EDX Automation Testing School');
+      cy.get(selectors.dashboard.schoolDetailsCard).click();
+      cy.get(selectors.schoolDetails.addWebsiteLink).click();
 
-    websiteField().clear().type('http://www.nope.com');
-    websiteWrapper().should('have.class', 'v-input--error');
-    websiteWrapper().within(() =>
-      cy.get('.v-messages__message').should('contain.text', websiteErrorMessage)
-    );
+      const websiteField = () => cy.get(selectors.schoolDetails.schoolDetailsWebsite);
+      const websiteWrapper = vInputParentOf(websiteField);
+      const websiteErrorMessage = 'Website must be valid and secure (i.e., https)';
 
-    websiteField().clear().type('https://notawebsite');
-    websiteWrapper().should('have.class', 'v-input--error');
-    websiteWrapper().within(
-      () => cy.get('.v-messages__message').should('contain.text', websiteErrorMessage)
-    );
+      websiteField().clear().type('http://www.nope.com');
+      websiteWrapper().should('have.class', 'v-input--error');
+      websiteWrapper().within(() =>
+        cy.get('.v-messages__message').should('contain.text', websiteErrorMessage)
+      );
 
-    websiteField().clear().type('https://saulgoodman.com');
-    websiteWrapper().should('not.have.class', 'v-input--error');
-    websiteWrapper().within(() => cy.get('.v-messags__message').should('not.exist'));
+      websiteField().clear().type('https://notawebsite');
+      websiteWrapper().should('have.class', 'v-input--error');
+      websiteWrapper().within(
+        () => cy.get('.v-messages__message').should('contain.text', websiteErrorMessage)
+      );
+
+      websiteField().clear().type('https://saulgoodman.com');
+      websiteWrapper().should('not.have.class', 'v-input--error');
+      websiteWrapper().within(() => cy.get('.v-messags__message').should('not.exist'));
+    });
   });
 });
