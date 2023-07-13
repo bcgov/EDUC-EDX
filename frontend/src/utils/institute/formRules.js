c38b98b51e56b900fa465aec56579735a0a7c72a
mightycox <j.acox@hotmail.com>
2023-07-13 13:18:09 -0700

    EDX-1409 - Changes the error message for special characters

--- frontend/src/utils/institute/formRules.js
+++ frontend/src/utils/institute/formRules.js
@@ -38,13 +38,14 @@ const number = (message = 'Must be a number') => {
  * @param {String} [message]
  * @returns {(value: string) => true|string}
  */
-const noSpecialCharactersAddress = (message = 'Remove or replace any special characters in this field.') =>
+const specialCharacterMessage = 'Special characters currently aren’t accepted, but we recognize their importance and are working on an update. For now, please remove or replace them.';
+const noSpecialCharactersAddress = (message = specialCharacterMessage) =>
   v => !v || !/[^A-Za-z0-9\s-.#/]/.test(v) || message;
 
-const noSpecialCharactersContactTitle = (message = 'Remove or replace any special characters in this field.') =>
+const noSpecialCharactersContactTitle = (message = specialCharacterMessage) =>
   v => !v || !/[^A-Za-z.'\s-&()]/.test(v) || message;
 
-const noSpecialCharactersContactName = (message = 'Remove or replace any special characters in this field.') =>
+const noSpecialCharactersContactName = (message = specialCharacterMessage) =>
   v => !v || !/[^A-Za-z.'\s-]/.test(v) || message;
 /**
  * Rule for phone numbers also works for fax numbers too
