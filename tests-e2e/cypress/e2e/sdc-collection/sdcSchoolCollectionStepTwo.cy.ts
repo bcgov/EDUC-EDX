65ae8c0e7a3da7f7589328f0604f7a53749ed6d3
ANSINGH <angadh.singh1@gmail.com>
2023-08-25 14:49:20 -0700

    EDX-1143 intial commit

--- tests-e2e/cypress/e2e/sdc-collection/sdcSchoolCollectionStepTwo.cy.ts
+++ tests-e2e/cypress/e2e/sdc-collection/sdcSchoolCollectionStepTwo.cy.ts
@@ -14,7 +14,7 @@ describe('SDC School Collection View', () => {
         cy.task<SchoolUserOptions, EdxUserEntity>('setup-schoolUser', { schoolCodes: ['99998'] });
       });
     });
-    after(() => cy.logout());
+    // after(() => cy.logout());
     beforeEach(() => cy.login());
 
     it('can load dashboard & click data collection card & process collection', () => {
@@ -45,6 +45,39 @@ describe('SDC School Collection View', () => {
 
       cy.get(selectors.studentLevelData.nextButton).should('not.be.disabled');
       cy.get(selectors.studentLevelData.nextButton).click();
+    });
+
+    it.only('can remove record containing validation errors', () => {
+        cy.intercept(Cypress.env('interceptors').collection_students_pagination).as('pagination');
+
+        cy.visit('/');
+        cy.get(selectors.dashboard.title).contains('Dashboard | EDX Automation Testing School');
+        cy.get(selectors.dashboard.dataCollectionsTileTitle).contains('Data Collections');
+        cy.get(selectors.dashboard.dataCollectionsTile).click();
+
+        cy.get(selectors.dataCollectionsLanding.title).should('exist').contains('Student Level Data (1701) | EDX Automation Testing School');
+        cy.get(selectors.dataCollectionsLanding.continue).contains('Continue').click();
+
+        cy.wait('@pagination').then(({response}) => {
+            cy.get(selectors.studentLevelData.nextButton).should('be.disabled');
+        })
+
+        // cy.get(selectors.studentLevelData.saveAndRefreshButton).click();
+
+        // cy.wait('@pagination').then(({response}) => {
+        //     cy.get(selectors.studentLevelData.nextButton).should('not.be.disabled');
+        //     cy.get(selectors.studentLevelData.legalLastNameValidationTextInput).should('not.exist');
+        //     cy.get(selectors.studentLevelData.postalCodeValidationTextInput).should('exist');
+        // })
+
+        // cy.get(selectors.studentLevelData.nextButton).should('not.be.disabled');
+        // cy.get(selectors.studentLevelData.nextButton).click();
+
+        cy.get(selectors.sdcSchoolStudentCollection.sdcCollectionStepTwo.removeRecord).should('exist');
+        cy.get(selectors.sdcSchoolStudentCollection.sdcCollectionStepTwo.removeRecord).click();
+        cy.get(selectors.sdcSchoolStudentCollection.sdcCollectionStepTwo.removeRecordRejectButton).click();
+        cy.get(selectors.sdcSchoolStudentCollection.sdcCollectionStepTwo.removeRecord).click();
+        cy.get(selectors.sdcSchoolStudentCollection.sdcCollectionStepTwo.removeRecordConfirmButton).click();
     });
   });
 });
