007e8438805b16ad9439680d175c8141220ceaeb
SoLetsDev <74216496+SoLetsDev@users.noreply.github.com>
2023-12-27 11:28:40 -0800

    Adding info warning and funding warning icons to details tabs.

--- frontend/src/components/common/CustomTable.vue
+++ frontend/src/components/common/CustomTable.vue
@@ -57,7 +57,14 @@
               v-if="column.title === 'select'"
               :model-value="isSelected(props.item.raw) !== undefined"
               @click="onClick(props)"
-            /> 
+            />
+            <v-icon
+              v-else-if="column.key === 'sdcSchoolCollectionStudentStatusCode'"
+              size="25"
+              :color="getSdcStudentStatusIconColor(props.item.raw['sdcSchoolCollectionStudentStatusCode'])"
+            >
+              {{ getSdcStudentIssueIcon(props.item.raw['sdcSchoolCollectionStudentStatusCode']) }}
+            </v-icon>
             <div v-else>
               <div v-if="column.key === 'legalName'">
                 <span v-if="props.item.raw['legalLastName']">{{ props.item.raw['legalLastName'] }}</span>,
@@ -183,7 +190,22 @@ export default {
       }
       this.masterCheckbox = this.selected.length > 0 && this.isAllSelected();
     },
-      
+    getSdcStudentStatusIconColor(status) {
+      if (status === 'FUNDWARN') {
+        return 'blue';
+      }
+      else if (status === 'INFOWARN') {
+        return 'orange';
+      }
+    },
+    getSdcStudentIssueIcon(status) {
+      if (status === 'FUNDWARN') {
+        return 'mdi-alert-outline';
+      }
+      else if (status === 'INFOWARN') {
+        return 'mdi-alert-circle-outline';
+      }
+    },
   }
 };
 </script>
--- frontend/src/utils/sdc/TableConfiguration.js
+++ frontend/src/utils/sdc/TableConfiguration.js
@@ -3,6 +3,7 @@ export const FTE = Object.freeze(
     defaultFilter: {label: 'DEFAULT_VALUE' ,description: ''},
     tableHeaders: [
       { title: 'select', key: 'select' },
+      { key: 'sdcSchoolCollectionStudentStatusCode'},
       { title: 'FTE', key: 'fte', align:'start' },
       { title: 'PEN', key: 'studentPen', subHeader: {title: 'Local ID', key: 'localID'}},
       { title: 'Legal Surname, Given (Middle)', key: 'legalName', subHeader: {title: 'Usual Surname, Given (Middle)', key: 'usualName'}},
@@ -19,6 +20,7 @@ export const FRENCH_PR = Object.freeze(
     defaultFilter: {label: 'FRENCH_PR', description:'Has French Program', enrolledProgramCodeValues: '05,08,11,14'},
     tableHeaders: [
       { title: 'select', key: 'select' },
+      { key: 'sdcSchoolCollectionStudentStatusCode'},
       { title: 'FTE', key: 'fte', align:'start', subHeader: {title: 'Program Eligible', key: 'frenchProgramEligible'} },
       { title: 'PEN', key: 'studentPen', subHeader: {title: 'Local ID', key: 'localID'}},
       { title: 'Legal Surname, Given (Middle)', key: 'legalName', subHeader: {title: 'Usual Surname, Given (Middle)', key: 'usualName'}},
@@ -35,6 +37,7 @@ export const CAREER_PR = Object.freeze(
     defaultFilter: {label: 'CAREER_PR' ,description: 'Has Career Program', enrolledProgramCodeValues: '40,41,42,43'},
     tableHeaders: [
       { title: 'select', key: 'select' },
+      { key: 'sdcSchoolCollectionStudentStatusCode'},
       { title: 'FTE', key: 'fte', align:'start', subHeader: {title: 'Program Eligible', key: 'careerProgramEligible'} },
       { title: 'PEN', key: 'studentPen', subHeader: {title: 'Local ID', key: 'localID'}},
       { title: 'Legal Surname, Given (Middle)', key: 'legalName', subHeader: {title: 'Usual Surname, Given (Middle)', key: 'usualName'}},
@@ -50,6 +53,7 @@ export const INDSUPPORT_PR = Object.freeze(
     defaultFilter: {label: 'INDSUPPORT_PR', description: 'Has Indigenous Support Program', enrolledProgramCodeValues: '29,33,36'},
     tableHeaders: [
       { title: 'select', key: 'select' },
+      { key: 'sdcSchoolCollectionStudentStatusCode'},
       { title: 'FTE', key: 'fte', align:'start', subHeader: {title: 'Program Eligible', key: 'indProgramEligible'} },
       { title: 'PEN', key: 'studentPen', subHeader: {title: 'Local ID', key: 'localID'}},
       { title: 'Legal Surname, Given (Middle)', key: 'legalName', subHeader: {title: 'Usual Surname, Given (Middle)', key: 'usualName'}},
@@ -66,6 +70,7 @@ export const SPECIALED_PR = Object.freeze(
     defaultFilter: {label: 'SPECIALED_PR', description: 'Has Special Education Category', spedCodeValues: 'A,B,C,D,E,F,G,H,K,P,Q,R'},
     tableHeaders: [
       { title: 'select', key: 'select' },
+      { key: 'sdcSchoolCollectionStudentStatusCode'},
       { title: 'FTE', key: 'fte', align:'start', subHeader: {title: 'Program Eligible', key: 'spedProgramEligible'} },
       { title: 'PEN', key: 'studentPen', subHeader: {title: 'Local ID', key: 'localID'}},
       { title: 'Legal Surname, Given (Middle)', key: 'legalName', subHeader: {title: 'Usual Surname, Given (Middle)', key: 'usualName'}},
@@ -81,6 +86,7 @@ export const ELL = Object.freeze(
     defaultFilter: {label: 'ELL', description: 'English Language Learner' },
     tableHeaders: [
       { title: 'select', key: 'select' },
+      { key: 'sdcSchoolCollectionStudentStatusCode'},
       { title: 'FTE', key: 'fte', align:'start', subHeader: {title: 'Program Eligible', key: 'programEligible'} },
       { title: 'PEN', key: 'studentPen', subHeader: {title: 'Local ID', key: 'localID'}},
       { title: 'Legal Surname, Given (Middle)', key: 'legalName', subHeader: {title: 'Usual Surname, Given (Middle)', key: 'usualName'}},
@@ -96,6 +102,7 @@ export const REFUGEE = Object.freeze(
     defaultFilter: { label: 'REFUGEE', description: '16-Newcomer Refugee'},
     tableHeaders: [
       { title: 'select', key: 'select' },
+      { key: 'sdcSchoolCollectionStudentStatusCode'},
       { title: 'FTE', key: 'fte', align:'start', subHeader: {title: 'Funding Eligible', key: 'fundingEligible'} },
       { title: 'PEN', key: 'studentPen', subHeader: {title: 'Local ID', key: 'localID'}},
       { title: 'Legal Surname, Given (Middle)', key: 'legalName', subHeader: {title: 'Usual Surname, Given (Middle)', key: 'usualName'}},
