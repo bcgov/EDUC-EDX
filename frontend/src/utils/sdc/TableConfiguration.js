043520d6e23a89dc6e8fb005c1c9ecdad27efede
Trevor Richards <trev@trevdev.ca>
2024-01-08 11:50:55 -0800

    fix: access valid key for ELL program eligibity in DetailComponent

--- frontend/src/utils/sdc/TableConfiguration.js
+++ frontend/src/utils/sdc/TableConfiguration.js
@@ -350,7 +350,7 @@ export const ELL = Object.freeze(
     tableHeaders: [
       { title: 'select', key: 'select' },
       { key: 'sdcSchoolCollectionStudentStatusCode'},
-      { title: 'FTE', key: 'fte', align:'start', subHeader: {title: 'Program Eligible', key: 'programEligible'} },
+      { title: 'FTE', key: 'fte', align:'start', subHeader: {title: 'Program Eligible', key: 'ellProgramEligible'} },
       { title: 'PEN', key: 'studentPen', subHeader: {title: 'Local ID', key: 'localID'}},
       { title: 'Legal Surname, Given (Middle)', key: 'legalName', subHeader: {title: 'Usual Surname, Given (Middle)', key: 'usualName'}},
       { title: 'Adult', key: 'isAdult', subHeader: {title: 'Grad', key: 'isGraduated'}},
