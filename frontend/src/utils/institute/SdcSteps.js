2843d98de7e6c5ef2d9070202ffa8299c41d1c14
soletsdev <soletsdev@github>
2024-01-10 08:19:24 -0800

    implementing stepper component

--- frontend/src/utils/institute/SdcSteps.js
+++ frontend/src/utils/institute/SdcSteps.js
@@ -1,61 +1,37 @@
-
 /**
- * stepStatus values: 'in-progress', 'not-started', 'complete'
+ * @type {({sdcSchoolCollectionStatusCode: string, index: number, title: string}|{sdcSchoolCollectionStatusCode: string, index: number, title: string}|{sdcSchoolCollectionStatusCode: string, index: number, title: string}|{sdcSchoolCollectionStatusCode: string, index: number, title: string}|{sdcSchoolCollectionStatusCode: string, index: number, title: string})[]}
+ * index refers to the stepper window index which starts at 0
+ * example: step-1 of the collection process is at index 0
  */
-export const SDC_STEPS = Object.freeze(
-  [
-    {
-      label: 'STEP-1',
-      name: 'Upload Data',
-      id: 'step-1',
-      route: 'step-1',
-      next: 'step-2',
-      index: 0,
-      isStarted: false,
-      isComplete: false
-    }, {
-      label: 'STEP-2',
-      name: 'Review & Fix Data Issues',
-      id: 'step-2',
-      route: 'step-2',
-      next: 'step-3',
-      index: 1,
-      isStarted: false,
-      isComplete: false
-    }, {
-      label: 'STEP-3',
-      name: 'Edit/Verify Data',
-      id: 'step-3',
-      route: 'step-3',
-      next: 'step-4',
-      index: 2,
-      isStarted: false,
-      isComplete: false
-    }, {
-      label: 'STEP-4',
-      name: 'Verify School Details (1601)',
-      id: 'step-4',
-      route: 'step-4',
-      next: 'step-5',
-      index: 3,
-      isStarted: false,
-      isComplete: false
-    }, {
-      label: 'STEP-5',
-      name: 'Verify School Contacts(1601)',
-      id: 'step-5',
-      index: 4,
-      route: 'step-5',
-      isStarted: false,
-      isComplete: false
-    }, {
-      label: 'STEP-6',
-      name: 'Submit Data',
-      id: 'step-6',
-      index: 5,
-      route: 'step-5',
-      isStarted: false,
-      isComplete: false
-    }
-  ]
-);
+export const SDC_STEPS_SCHOOL = Object.freeze([
+  {
+    title: 'Upload Data',
+    index: 0,
+    sdcSchoolCollectionStatusCode: 'NEW'
+  },
+  {
+    title: 'Review & Fix Data Issues',
+    index: 1,
+    sdcSchoolCollectionStatusCode: 'LOADED'
+  },
+  {
+    title: 'Edit/Verify Data Issues',
+    index: 2,
+    sdcSchoolCollectionStatusCode: 'REVIEWED'
+  },
+  {
+    title: 'Verify School Details (1601)',
+    index: 3,
+    sdcSchoolCollectionStatusCode: 'SCH_D_VRFD'
+  },
+  {
+    title: 'Verify School Contacts (1601)',
+    index: 4,
+    sdcSchoolCollectionStatusCode: 'SCH_C_VRFD'
+  },
+  {
+    title: 'Submit Data',
+    index: 5,
+    sdcSchoolCollectionStatusCode: 'TO_BE_CONFIRMED' //confirm this step when implemented
+  },
+]);
