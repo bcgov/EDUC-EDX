f4b79fa3298339ac6f2512f9ecf37ee145234b43
alexmcdermid <alexander.mcdermid@alumni.ubc.ca>
2024-01-25 15:48:16 -0800

    feat: test funding eligible filter

--- tests-e2e/cypress/e2e/sdc-collection/sdcSchoolCollectionStepThree.cy.ts
+++ tests-e2e/cypress/e2e/sdc-collection/sdcSchoolCollectionStepThree.cy.ts
@@ -268,6 +268,21 @@ describe('SDC School Collection View', () => {
 
       cy.get(selectors.specialEducationComponent.tab).contains('Filters').click();
       cy.get(selectors.activeFiltersDrawer.drawer).contains('Clear All Filters').click();
+      cy.get(selectors.activeFiltersDrawer.drawer).contains('Funding Eligible').click();
+      cy.get(selectors.activeFiltersDrawer.drawer).contains('Apply Filters').click();
+
+      cy.get(selectors.studentLevelData.detailsLoadingBar).should('exist');
+      cy.get(selectors.specialEducationComponent.tab).find(selectors.studentLevelData.studentsFound).should('exist').contains(2);
+      cy.get(selectors.specialEducationComponent.tab).find('tbody tr').each($cell => {
+        cy.wrap($cell).children().last().invoke('text').then((text) => {
+          expect(text).to.satisfy((value: string) => {
+            return value === 'A-Physically Dependent' || value === 'G-Autism Spectrum Disorder';
+          });
+        });
+      });
+
+      cy.get(selectors.specialEducationComponent.tab).contains('Filters').click();
+      cy.get(selectors.activeFiltersDrawer.drawer).contains('Clear All Filters').click();
       cy.get(selectors.activeFiltersDrawer.drawer).contains('Apply Filters').click();
 
       //check summary headcounts
