d674e7006fa096e43bb07c6a6db4d49ef8103ca5
Trevor Richards <trev@trevdev.ca>
2024-01-25 09:12:11 -0800

    revert: add nodejs route for years-in-ell
    This reverts commit 60f534c3e971042f0345a473a411e9a4270922a2.

--- backend/src/routes/sdc.js
+++ backend/src/routes/sdc.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { getCollectionBySchoolId, uploadFile, getSdcFileProgress, updateSchoolCollection, getSchoolCollectionById,
   getSDCSchoolCollectionStudentPaginated, getSDCSchoolCollectionStudentSummaryCounts,
   getSDCSchoolCollectionStudentDetail, updateAndValidateSdcSchoolCollectionStudent, deleteSDCSchoolCollectionStudent,
-  getStudentHeadcounts, createYearsInEll} = require('../components/sdc');
+  getStudentHeadcounts} = require('../components/sdc');
 const {getCachedSDCData} = require('../components/sdc-cache');
 const auth = require('../components/auth');
 const constants = require('../util/constants');
@@ -34,7 +34,7 @@ router.get('/sdcSchoolCollectionStudent/stats/error-warning-count/:sdcSchoolColl
 router.get('/sdcSchoolCollectionStudent/:sdcSchoolCollectionStudentID', passport.authenticate('jwt', {session: false}, undefined), isValidBackendToken, getSDCSchoolCollectionStudentDetail);
 router.put('/sdcSchoolCollectionStudent/:sdcSchoolCollectionID/student/:sdcSchoolCollectionStudentID', passport.authenticate('jwt', {session: false}, undefined), isValidBackendToken, updateAndValidateSdcSchoolCollectionStudent);
 router.delete('/sdcSchoolCollectionStudent/:sdcSchoolCollectionID/student/:sdcSchoolCollectionStudentID', passport.authenticate('jwt', {session: false}, undefined), isValidBackendToken, deleteSDCSchoolCollectionStudent);
-router.post('/sdcSchoolCollectionStudent/years-in-ell', passport.authenticate('jwt', {session: false}, undefined), isValidBackendToken, createYearsInEll);
+
 router.get('/sdcSchoolCollectionStudent/getStudentHeadcounts/:sdcSchoolCollectionID', passport.authenticate('jwt', {session: false}, undefined), isValidBackendToken, getStudentHeadcounts);
 
 module.exports = router;
