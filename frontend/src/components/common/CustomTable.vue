2bfa2b79d39fb66132ec45eaa3d204771463044d
alexmcdermid <alexander.mcdermid@alumni.ubc.ca>
2024-03-04 16:16:27 -0800

    step 3 pen column to assigned pen and show under review if no assigned pen in table

--- frontend/src/components/common/CustomTable.vue
+++ frontend/src/components/common/CustomTable.vue
@@ -32,6 +32,17 @@
                 @change="toggle()"
               />
             </div>
+            <div v-else-if="column.title === 'PEN'">
+              <div class="header-text">
+                Assigned PEN
+              </div>
+              <div
+                v-if="column.hasOwnProperty('subHeader')"
+                class="header-text"
+              >
+                {{ column.subHeader.title }}
+              </div>
+            </div>
             <div v-else>
               <div class="header-text">
                 {{ column.title }}
@@ -78,7 +89,11 @@
               {{ getSdcStudentStatusHoverText(props.item.raw['sdcSchoolCollectionStudentStatusCode']) }}
             </v-tooltip>
             <div v-else>
-              <span v-if="column.key === 'legalName'">
+              <span v-if="column.key === 'studentPen'">
+                {{ getAssignedPen(props.item.raw['assignedPen']) }}
+              </span>
+
+              <span v-else-if="column.key === 'legalName'">
                 {{ displayName(props.item.raw['legalFirstName'], props.item.raw['legalMiddleNames'], props.item.raw['legalLastName']) }}
               </span>
 
@@ -248,6 +263,13 @@ export default {
         return 'Funding Warning';
       } else if (status === 'INFOWARN') {
         return 'Info Warning';
+      }
+    },
+    getAssignedPen(assignedPen) {
+      if (assignedPen) {
+        return assignedPen;
+      } else {
+        return 'under review';
       }
     },
     displayName(first, middle, last) {
