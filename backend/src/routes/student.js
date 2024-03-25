b98431a9ff28a5603f58f56eabf57c5f2eac4ee7
keeganbcgov <Keegan.Laur@gov.bc.ca>
2024-03-25 13:58:27 -0700

    EDX-2356: added some more permission checking.

--- backend/src/routes/student.js
+++ backend/src/routes/student.js
@@ -4,11 +4,12 @@ const { getStudentByPEN } = require('../components/student');
 const auth = require('../components/auth');
 const passport = require('passport');
 const isValidBackendToken = auth.isValidBackendToken();
-const { validateAccessToken } = require('../components/permissionUtils');
+const { validateAccessToken, checkEdxUserPermission } = require('../components/permissionUtils');
+const { PERMISSION } = require('../util/Permission');
 
 /*
  * Get student details based on PEN Number
  */
-router.get('/', passport.authenticate('jwt', {session: false}, undefined), isValidBackendToken, validateAccessToken, getStudentByPEN);
+router.get('/', passport.authenticate('jwt', {session: false}, undefined), isValidBackendToken, validateAccessToken, checkEdxUserPermission(PERMISSION.SECURE_EXCHANGE), getStudentByPEN);
 
 module.exports = router;
