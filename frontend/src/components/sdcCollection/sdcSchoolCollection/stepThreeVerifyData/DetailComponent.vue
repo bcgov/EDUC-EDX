2d3bfd3acbbe4349485dac3f4c8054d785328589
Eckerman <erineckerman@gmail.com>
2024-05-29 12:44:33 -0700

    Remove bulk edit button

--- frontend/src/components/sdcCollection/sdcSchoolCollection/stepThreeVerifyData/DetailComponent.vue
+++ frontend/src/components/sdcCollection/sdcSchoolCollection/stepThreeVerifyData/DetailComponent.vue
@@ -52,15 +52,6 @@
             @click="removeStudents"
           />
           <v-btn
-            id="bulkEdit"
-            color="#003366"
-            class="mr-1 mb-1"
-            text="Bulk Edit"
-            prepend-icon="mdi-pencil-outline"
-            variant="outlined"
-            :disabled="selectedStudents.length < 2 || schoolCollection?.sdcSchoolCollectionStatusCode === 'SUBMITTED'"
-          />
-          <v-btn
             id="filters"
             color="#003366"
             text="Filter"
