4efe85c09fe5cf34f5765b70a76680a43313eb4a
keeganbcgov <Keegan.Laur@gov.bc.ca>
2024-06-05 16:28:20 -0700

    EDX-2404: UI changes around enrollment duplicates.

--- frontend/src/components/sdcCollection/sdcDistrictCollection/stepFourInDistrictDuplicates/EnrollmentDuplicateResolveViaRemove.vue
+++ frontend/src/components/sdcCollection/sdcDistrictCollection/stepFourInDistrictDuplicates/EnrollmentDuplicateResolveViaRemove.vue
@@ -1,124 +1,46 @@
 <template>
-  <v-card
-    id="enrollmentDuplicate"
-  >
-    <v-card-title
-      id="enrollmentDuplicateTitle"
-      class="sheetHeader pt-1 pb-1"
-    >
-      <v-row no-gutters>
-        <v-col class="d-flex justify-start">
-          Remove Student
-        </v-col>
-        <v-col class="d-flex justify-end">
-          <v-btn
-            id="cancel"
-            color="white"
-            text="Close"
-            size="30"
-            icon="mdi-close"
-            variant="tonal"
-            @click="close"
-          />
-        </v-col>
-      </v-row>
-    </v-card-title>
-    <v-divider />
-    <v-card-text>
-      Are you sure you want to remove {{ displayName(sdcSchoolCollectionStudent.legalFirstName, sdcSchoolCollectionStudent.legalMiddleNames, sdcSchoolCollectionStudent.legalLastName) }} from {{ sdcSchoolCollectionStudent.schoolName }}?
-    </v-card-text>
-    <v-card-actions>
-      <VSpacer />
-      <PrimaryButton
-        id="cancelResolveDialog"
-        secondary
-        text="Cancel"
-        :click-action="close"
-      />
-      <PrimaryButton
-        id="actionResolve"
-        text="Remove"
-        :click-action="removeAndResolve"
-      />
-    </v-card-actions>
-  </v-card>
+  <ConfirmationDialog ref="confirmResolveEnrollmentDuplicateViaRemoveStudent">
+    <template #message>
+      <p>Are you sure you want to remove {{ displayName(sdcSchoolCollectionStudent.legalFirstName, sdcSchoolCollectionStudent.legalMiddleNames, sdcSchoolCollectionStudent.legalLastName) }} from {{ sdcSchoolCollectionStudent.schoolName }}?</p>
+    </template>
+  </ConfirmationDialog>
 </template>
 <script>
-  
-  
-  
-import PrimaryButton from '../../../util/PrimaryButton.vue';
 import {displayName} from '../../../../utils/format';
 import ApiService from '../../../../common/apiService';
 import {ApiRoutes} from '../../../../utils/constants';
-import {setFailureAlert, setSuccessAlert, setWarningAlert} from '../../../composable/alertComposable';
+import {setFailureAlert, setSuccessAlert} from '../../../composable/alertComposable';
+import ConfirmationDialog from '../../../util/ConfirmationDialog.vue';
 
 export default {
   name: 'EnrollmentDuplicateResolveViaRemove',
   components: {
-    PrimaryButton
+    ConfirmationDialog
   },
-  props: {
-    duplicate: {
-      type: Object,
-      required: true
-    },
-    sdcSchoolCollectionStudent: {
-      type: Object,
-      required: true
-    }
+  emits: ['close-refresh'],
+  data() {
+    return {
+      sdcSchoolCollectionStudent: {}
+    };
   },
-  emits: ['close', 'close-refresh'],
   methods: {
     displayName,
-    removeAndResolve() {
-      ApiService.apiAxios.post(`${ApiRoutes.sdc.SDC_DISTRICT_COLLECTION}/${this.$route.params.sdcDistrictCollectionID}/resolve-district-duplicates/${this.duplicate?.sdcDuplicateID}/DELETE_ENROLLMENT_DUPLICATE`, [this.sdcSchoolCollectionStudent])
-        .then((res) => {
-          if (res.data.sdcDuplicateID === this.selectedProgramDuplicate?.sdcDuplicateID && res.data.duplicateResolutionCode !== 'RELEASED') {
-            setWarningAlert('Warning! This update has created an error on the student record. Duplicate resolution will not be saved until all errors are resolved.');
-          } else {
-            setSuccessAlert('Success! The student has been removed.');
-            this.closeRefresh();
-          }
+    async removeAndResolveStudent(duplicate, sdcSchoolCollectionStudent) {
+      this.sdcSchoolCollectionStudent = sdcSchoolCollectionStudent;
+      const confirmation = await this.$refs.confirmResolveEnrollmentDuplicateViaRemoveStudent.open('Remove Student', null, {color: '#fff', width: 580, closeIcon: false, subtitle: false, dark: false, resolveText: 'Remove', rejectText: 'Cancel'});
+      if (!confirmation) {
+        return;
+      }
+      ApiService.apiAxios.post(`${ApiRoutes.sdc.SDC_DISTRICT_COLLECTION}/${this.$route.params.sdcDistrictCollectionID}/resolve-district-duplicates/${duplicate?.sdcDuplicateID}/DELETE_ENROLLMENT_DUPLICATE`, [this.sdcSchoolCollectionStudent])
+        .then(() => {
+          setSuccessAlert('Success! The student has been removed.');
         }).catch(error => {
           console.error(error);
           setFailureAlert(error?.response?.data?.message ? error?.response?.data?.message : 'An error occurred while trying to update student details. Please try again later.');
-          this.closeRefresh();
+        }).finally(() => {
+          this.$emit('close-refresh');
         });
-    },
-    close() {
-      this.$emit('close');
-    },
-    closeRefresh() {
-      this.$emit('close-refresh');
-    },
+    }
   }
 };
 </script>
-  
-  <style scoped>
-   .containerSetup{
-      padding-right: 0em !important;
-      padding-left: 0em !important;
-    }
-  
-    @media screen and (max-width: 1200px) {
-      .containerSetup{
-        padding-right: 3em !important;
-        padding-left: 3em !important;
-      }
-    }
-  
-    .success-message{
-      vertical-align: sub;
-     }
-  
-    .sheetHeader {
-      background-color: #003366;
-      color: white;
-      font-size: medium !important;
-      font-weight: bolder !important;
-    }
-  
-  </style>
-  
