d4c98a6e2e55f9a21660060aacd602db996572ae
Marco Villeneuve <arcshiftsolutions@gmail.com>
2024-06-06 13:31:17 -0700

    Small fix for download extension

--- backend/src/components/sdc.js
+++ backend/src/components/sdc.js
@@ -585,14 +585,14 @@ function getFileDetails(reportType, mincode) {
   const mappings = {
     'ALL_STUDENT_DIS_CSV': { filename: `AllDistrictStudents_${mincode}.csv`, contentType: 'text/csv' },
     'ALL_STUDENT_SCHOOL_CSV': { filename: `AllSchoolStudents_${mincode}.csv`, contentType: 'text/csv' },
-    'ELL_HEADCOUNT': { filename: `ELLHeadcount_${mincode}.csv`, contentType: 'application/pdf' },
-    'REFUGEE_HEADCOUNT': { filename: `RefugeeHeadcount_${mincode}.csv`, contentType: 'application/pdf' },
-    'SPECIAL_EDUCATION_HEADCOUNT': { filename: `SpecialEdHeadcount_${mincode}.csv`, contentType: 'application/pdf' },
-    'INDIGENOUS_HEADCOUNT': { filename: `IndigenousHeadcount_${mincode}.csv`, contentType: 'application/pdf' },
-    'BAND_RESIDENCE_HEADCOUNT': { filename: `BandOfResidenceHeadcount_${mincode}.csv`, contentType: 'application/pdf' },
-    'CAREER_HEADCOUNT': { filename: `CareerProgramsHeadcount_${mincode}.csv`, contentType: 'application/pdf' },
-    'FRENCH_HEADCOUNT': { filename: `FrenchProgramsHeadcount_${mincode}.csv`, contentType: 'application/pdf' },
-    'GRADE_ENROLLMENT_HEADCOUNT': { filename: `GradeEnrollmentHeadcount_${mincode}.csv`, contentType: 'application/pdf' },
+    'ELL_HEADCOUNT': { filename: `ELLHeadcount_${mincode}.pdf`, contentType: 'application/pdf' },
+    'REFUGEE_HEADCOUNT': { filename: `RefugeeHeadcount_${mincode}.pdf`, contentType: 'application/pdf' },
+    'SPECIAL_EDUCATION_HEADCOUNT': { filename: `SpecialEdHeadcount_${mincode}.pdf`, contentType: 'application/pdf' },
+    'INDIGENOUS_HEADCOUNT': { filename: `IndigenousHeadcount_${mincode}.pdf`, contentType: 'application/pdf' },
+    'BAND_RESIDENCE_HEADCOUNT': { filename: `BandOfResidenceHeadcount_${mincode}.pdf`, contentType: 'application/pdf' },
+    'CAREER_HEADCOUNT': { filename: `CareerProgramsHeadcount_${mincode}.pdf`, contentType: 'application/pdf' },
+    'FRENCH_HEADCOUNT': { filename: `FrenchProgramsHeadcount_${mincode}.pdf`, contentType: 'application/pdf' },
+    'GRADE_ENROLLMENT_HEADCOUNT': { filename: `GradeEnrollmentHeadcount_${mincode}.pdf`, contentType: 'application/pdf' },
     'DEFAULT': { filename: 'download.pdf', contentType: 'application/pdf' }
   };
   return mappings[reportType] || mappings['DEFAULT'];
