ed821af57c89d2220bfad86816843836b8fa1f76
alexmcdermid <alexander.mcdermid@alumni.ubc.ca>
2024-06-24 11:05:33 -0700

    check different steps and correct header titles

--- tests-e2e/cypress/e2e/sdc-collection/sdcSchoolCollection1601.cy.ts
+++ tests-e2e/cypress/e2e/sdc-collection/sdcSchoolCollection1601.cy.ts
@@ -28,13 +28,37 @@ describe('SDC School Collection View', () => {
     beforeEach(() => cy.login());
 
     it('can navigate to open school collection tile and see pie chart is different', () => {
-
       cy.visit('/');
       cy.get(selectors.dashboard.dataCollectionsTile).click();
-      // check that pie chart is diff
+      cy.get(selectors.collectionSummary.stepsComplete).should('exist').contains('1 / 9 Steps Complete');
+    });
+
+    it('can verify all stepper headers and their titles', () => {
+      cy.visit('/');
+      cy.get(selectors.dashboard.dataCollectionsTile).click();
       cy.get(selectors.dataCollectionsLanding.continue).contains('Continue').click();
 
+      interface Steps {
+        [key: string]: string;
+      }
 
+      const steps: Steps = {
+        step1: 'Upload Data',
+        step2: 'Review & Fix Data Issues',
+        step3: 'Edit/Verify Data Issues',
+        step4: 'Review & Fix Identical PENs',
+        step5: 'Verify School Details (1601)',
+        step6: 'Verify School Contacts (1601)',
+        step7: 'Submit Data',
+        step8: 'Resolve Province Duplicates',
+        step9: 'Review & Sign-Off Final Submission'
+      };
+      
+      Object.entries(steps).forEach(([stepId, expectedTitle]) => {
+        cy.get(selectors.independent1601.steps[stepId as keyof typeof selectors.independent1601.steps])
+          .should('exist')
+          .and('have.text', expectedTitle);
+      });
     });
   });
 });
--- tests-e2e/cypress/support/selectors.ts
+++ tests-e2e/cypress/support/selectors.ts
@@ -21,6 +21,10 @@ export default {
     headcountColumnData: '.career-headcount-column-data',
     headcountTableSubHeading: '.section-header'
   },
+  collectionSummary: {
+    stepsComplete: '.steps > span',
+    continue: '.navigate'
+  },
   dashboard: {
     dataCollectionsTile: '#studentDataCollectionCard',
     dataCollectionsTileTitle: '#studentDataCollectionCard > div.v-row.pl-4 > div.v-col.mt-2 > div:nth-child(1) > div > h4',
@@ -202,6 +206,20 @@ export default {
     codeXA: '#codeXA',
     refugeeFundingEligible: '#refugeeFundingEligible',
     refugeeFundingNotEligible: '#refugeeFundingNotEligible'
+  },
+  independent1601: {
+    stepper: '.v-stepper-header',
+    steps: {
+      step1: '#step-1 .v-stepper-item__title',
+      step2: '#step-2 .v-stepper-item__title',
+      step3: '#step-3 .v-stepper-item__title',
+      step4: '#step-4 .v-stepper-item__title',
+      step5: '#step-5 .v-stepper-item__title',
+      step6: '#step-6 .v-stepper-item__title',
+      step7: '#step-7 .v-stepper-item__title',
+      step8: '#step-8 .v-stepper-item__title',
+      step9: '#step-9 .v-stepper-item__title',
+    }
   },
   invitationSelection: {
     loginButtonBCeID: '#login-button-bceid',
