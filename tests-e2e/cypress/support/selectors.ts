124f4db475de43fdee070e3473d249bfc2ad9739
alexmcdermid <alexander.mcdermid@alumni.ubc.ca>
2024-08-22 15:56:15 -0700

    clean up

--- tests-e2e/cypress/support/selectors.ts
+++ tests-e2e/cypress/support/selectors.ts
@@ -25,9 +25,6 @@ export default {
     stepsComplete: '.steps > span',
     continue: '.navigate'
   },
-  completeCollection: {
-
-  },
   dashboard: {
     dataCollectionsTile: '#studentDataCollectionCard',
     dataCollectionsTileTitle: '#studentDataCollectionCard > div.v-row.pl-4 > div.v-col.mt-2 > div:nth-child(1) > div > h4',
