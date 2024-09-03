3a3ca3cfa36114d3fa0172ec40c859d5b6c024ce
QSL\SumathiT <sumathi.thirumani@quartech.com>
2024-09-03 09:29:29 -0700

    Changes to wrap the tab label for signoff.

--- frontend/src/utils/sdc/SdcVerifyTabs.js
+++ frontend/src/utils/sdc/SdcVerifyTabs.js
@@ -13,3 +13,22 @@ export const SDC_VERIFY_TABS = Object.freeze(
     'Refugee'
   ]
 );
+
+/**
+ * This is an supportive props object to allow changing the UX info for tabs.
+ */
+export const SDC_VERIFY_TAB_PROPS = Object.freeze(
+  [
+    {name: 'All Students', label:'All \n Students'},
+    {name: 'French Programs', label:'French \n Programs'},
+    {name: 'Career Programs', label:'Career \n Programs'},
+    {name: 'Indigenous Students & Support Programs', label:'Indigenous Students \n & Support Programs'},
+    {name: 'Inclusive Education', label:'Inclusive \n Education'},
+    {name: 'English Language Learning', label:'English Language \n Learning'},
+    {name: 'Refugee', label:'Refugee'},
+  ]
+);
+
+export function getSdcVerifyTabLabel(name) {
+  return SDC_VERIFY_TAB_PROPS.find(a => a.name === name)?.label;
+}
