83c083c3a15380664e8189a4f198c2d9e1e3c510
alexmcdermid <alexander.mcdermid@alumni.ubc.ca>
2024-09-18 13:56:28 -0700

    rename table constants to reflect value, enable 'under review' text for assigned pen on dis side

--- frontend/src/components/common/CustomTable.vue
+++ frontend/src/components/common/CustomTable.vue
@@ -90,7 +90,7 @@
               </div>
             </v-tooltip>
             <div v-else>
-              <span v-if="column.key === 'studentPen'">
+              <span v-if="column.key === 'assignedPen'">
                 {{ getAssignedPen(props.item['assignedPen']) }}
               </span>
 
