a0f457c256bb9630480ea922c87dbd9db430662d
Marco Villeneuve <arcshiftsolutions@gmail.com>
2024-11-25 15:37:45 -0800

    Public key pull.

--- tools/config/update-configmap.sh
+++ tools/config/update-configmap.sh
@@ -78,7 +78,7 @@ echo Fetching public key from SOAM
 fullKey=$(curl -sX GET "https://$SOAM_KC/auth/admin/realms/$SOAM_KC_REALM_ID/keys" \
   -H "Content-Type: application/json" \
   -H "Authorization: Bearer $TKN" \
-  | jq -r '.keys | .[] | select(has("publicKey")) | .publicKey')
+  | jq -r ".keys[] | select(.algorithm=="RS256") | .publicKey')
 
 echo Fetching public key from SOAM
 soamFullPublicKey="-----BEGIN PUBLIC KEY----- $fullKey -----END PUBLIC KEY-----"
