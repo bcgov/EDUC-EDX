f9aae569745c7f8bc154458c65c1f02129979ed8
Marco Villeneuve <arcshiftsolutions@gmail.com>
2025-04-30 14:23:37 -0700

    Changes for EAS name change

--- frontend/src/common/apiService.js
+++ frontend/src/common/apiService.js
@@ -183,7 +183,7 @@ export default {
   getAllCollectionTypeCodes: getCodes(`${ApiRoutes.sdc.COLLECTION_TYPE_CODES_URL}`),
   getGdcValidationFieldCodes: getCodes(`${ApiRoutes.gdc.VALIDATION_FIELD_CODES}`),
   getGradProgramCodes: getCodes(`${ApiRoutes.gdc.GRAD_PROGRAM_CODES}`),
-  getAllAssessmentSpecialCaseCodes:getCodes(`${ApiRoutes.assessment.GET_ASSESSMENT_SPECIALCASE_TYPES}`),
+  getAllAssessmentSpecialCaseCodes:getCodes(`${ApiRoutes.assessments.GET_ASSESSMENT_SPECIALCASE_TYPES}`),
 
 
 };
