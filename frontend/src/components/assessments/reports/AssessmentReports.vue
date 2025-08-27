1f7c4730798836600c7ed7e573ca91b3ad234a6b
Avisha Sodhi <38086281+SodhiA1@users.noreply.github.com>
2025-08-27 15:47:48 -0700

    Update AssessmentReports.vue

--- frontend/src/components/assessments/reports/AssessmentReports.vue
+++ frontend/src/components/assessments/reports/AssessmentReports.vue
@@ -243,7 +243,7 @@
               />
             </v-col>
           </v-row>
-          <div v-for="(assessment, index) in selectedAssessments">
+          <div v-for="(assessment) in selectedAssessments">
             <v-row class="pl-3 pb-3">
               <v-col 
                 cols="12"
